40c67998a15603f8fb31af746c3882c472c43660
guibermaia <guilhermebertoldi21384@gmail.com>
2020-11-26 22:08:10 -0300

    screen of login, install react-router-dom

---
+++ src/components/LoginComponent.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+import './login.css';
+
+import Login from '../pages/Login'
+
+function LoginComponent() {
+    return (
+        <Login />
+    );
+}
+
+export default LoginComponent;
